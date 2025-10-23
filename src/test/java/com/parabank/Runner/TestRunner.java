a27ad5667836369f1f7b2419a0e772ad1548b8a2
podshobosh <podsho1704@gmail.com>
2025-10-23 11:51:54 -0400

    Initial commit: BDD framework with database testing setup

---
+++ src/test/java/com/parabank/Runner/TestRunner.java
@@ -0,0 +1,24 @@
+package com.parabank.Runner;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "classpath:features",
+        glue = {"com/parabank/stepdefinitions", "hooks"},
+        plugin = {                                  // Report and output plugins
+                "pretty",                               // Prints Gherkin steps in the console
+                "html:target/cucumber-reports.html",    // Generates an HTML report
+                "json:target/cucumber.json",            // Generates a JSON report
+                "junit:target/cucumber.xml"             // Generates a JUnit-compatible XML report
+        },
+//        monochrome = false,
+        tags = "@Testcase002",
+        dryRun = false
+)
+public class TestRunner {
+
+}
