a82bc04aba3642d7466016c411410f2578b595a7
podshobosh <poddsho1704@gmail.com>
2026-03-04 12:27:44 -0500

    corrected the App Url

--- src/main/java/utils/AppUrls.java
+++ src/main/java/utils/AppUrls.java
@@ -1,11 +1,11 @@
 package utils;
 
 public class AppUrls {
-    public static final String HOME_PAGE = "https://parabank.parasoft.com/parabank/index.htm";
-    public static final String ABOUT_US_PAGE = "https://parabank.parasoft.com/parabank/about.htm";
-    public static final String SERVICES_PAGE = "https://parabank.parasoft.com/parabank/services.htm";
+    public static final String HOME_PAGE = "http://localhost:8080/parabank/index.htm";
+    public static final String ABOUT_US_PAGE = "http://localhost:8080/parabank/about.htm";
+    public static final String SERVICES_PAGE = "http://localhost:8080/parabank/services.htm";
     public static final String PRODUCTS_PAGE = "https://www.parasoft.com/products/";
     public static final String LOCATIONS_PAGE = "https://www.parasoft.com/solutions/";
-    public static final String ADMIN_PAGE = "https://parabank.parasoft.com/parabank/admin.htm";
+    public static final String ADMIN_PAGE = "http://localhost:8080/parabank/admin.htm";
 
 }
